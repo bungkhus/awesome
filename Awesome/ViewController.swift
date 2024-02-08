81d454babc4cc2babe97b300c88c9110cda4b2c1
Muhammad Khusnan <muhammad.khusnan@gmail.com>
2024-02-08 19:16:45 +0700

    initial project

---
+++ Awesome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Awesome
+//
+//  Created by Lion Parcel on 08/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
